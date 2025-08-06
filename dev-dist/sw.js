f3a5200fb11bb20e7192a89b2e42a7b8a60b2e65
DemonVenom666 <demonvenom9@gmail.com>
2025-08-06 20:58:48 +0200

    create Home page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.60tnov5md9"
+    "revision": "0.589bahd3trg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import css from './Home.module.css';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={css.container}>
+      <img src="/logo.png" alt="Golden Snack logo" className={css.logo} />
+      <h1 className={css.title}>Vítejte v konfigurátoru Golden Snack</h1>
+      <p className={css.subtitle}>
+        Vyberte si stroj, který chcete upravit nebo prozkoumat.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
