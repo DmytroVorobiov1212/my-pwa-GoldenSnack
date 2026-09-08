5252f4166bc55ba149490b28feb2b3151c0a4a4d
Dmytro Vorobiov <116897771+DmytroVorobiov1212@users.noreply.github.com>
2026-09-08 20:38:20 +0200

    feat: display managed Masek production cards

--- src/pages/Masek/Masek.jsx
+++ src/pages/Masek/Masek.jsx
@@ -1,29 +1,10 @@
-import { Link } from 'react-router-dom';
-import css from './Masek.module.css';
+import ButlerList from '../../components/ButlerList/ButlerList';
+import { useProductionCards } from '../../production/useProductionCards';
 
 const Masek = () => {
-  return (
-    <section className={css.container}>
-      <div className={css.topRow}>
-        <Link to="/balicka" className={css.backButton}>‹ Baličky</Link>
-        <span className={css.sectionLabel}>MAŠEK</span>
-      </div>
+  const { cards } = useProductionCards('masek', [], { allowEmptyServer: true });
 
-      <div className={css.card}>
-        <div className={css.badge}>03</div>
-        <h1>Mašek</h1>
-        <p>
-          Balička je už součástí výrobního terminálu. Konkrétní parametry a
-          výrobky doplníme podle skutečného nastavení stroje.
-        </p>
-
-        <div className={css.statusBox}>
-          <strong>Konfigurátor připraven k doplnění</strong>
-          <span>Nebudeme zadávat žádné hodnoty bez ověření na stroji.</span>
-        </div>
-      </div>
-    </section>
-  );
+  return <ButlerList data={cards} />;
 };
 
 export default Masek;
